6dbbabb1e57e251560519fecf04c26c28fc5d682
Akhil Singh Rana <akhil.singh.rana.airbus@gmail.com>
2019-08-27 18:53:34 +0200

    CNN model added

--- CNN/model.py
+++ CNN/model.py
@@ -17,3 +17,18 @@ import os
 #import other modules
 import numpy as np
 
+class CNN_model():
+    
+    # Initialize the class, with some default variables
+    def __init__(self, args=None):
+        """
+            args = Argument parser from the options.py file
+        """
+        self.args = args
+
+    # This function defines the CNN architecture
+    def cnn_model(self):
+        model = Sequential()
+        model.add(layers.Conv2d(filters))
+
+        return model
