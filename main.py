41435e6612dfc815f7753ac9976f5e13396d3b1a
Tatjana Bürgmann <tatjana.buergmann@gmail.com>
2019-09-03 18:45:59 +0200

    Now support matplot lib ploting and testing

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from tensorflow import keras as keras
 import tensorflow_hub as hub
 
 import numpy as np
+import matplotlib.pylab as plt
+
 import options
 
 def train(args):
@@ -55,8 +57,37 @@ def test(args):
     test_data_gen = dataloader.dataGenerator()
 
     #if use PreTrain flag is enabled
-    if args.usePreTrain:
-        reloaded = tf.keras.experimental.load_from_saved_model(args.load_dir, custom_objects={'KerasLayer':hub.KerasLayer})
+    if not args.usePreTrain:
+        reloaded = keras.experimental.load_from_saved_model(args.load_dir, custom_objects={'KerasLayer':hub.KerasLayer})
+        reloaded.summary()
+        
+        class_names = sorted(test_data_gen.class_indices.items(), key=lambda pair:pair[1])
+        class_names = np.array([key.title() for key, value in class_names])
+        
+        print(class_names)
+        
+        #Do the prediction of batches
+        for image_batch, label_batch in test_data_gen:
+            predicted_batch = reloaded.predict(image_batch)
+            predicted_id = np.argmax(predicted_batch, axis=-1)
+            predicted_label_batch = class_names[predicted_id]
+
+            print("Predicted -->", predicted_label_batch)
+            # Actual label id
+            label_id = np.argmax(label_batch, axis=-1)
+            print("Real GT Batch-->", label_id)
+
+            plt.figure(figsize=(10,9))
+            plt.subplots_adjust(hspace=0.5)
+            for n in range(30):
+                plt.subplot(6,5,n+1)
+                plt.imshow(image_batch[n])
+                color = "green" if predicted_id[n] == label_id[n] else "red"
+                plt.title(predicted_label_batch[n].title(), color=color)
+                plt.axis('off')
+                _ = plt.suptitle("Model predictions (green: correct, red: incorrect)")
+            plt.show()
+            
 
 
 #use Pre Trained MOdels for Fine Tuning
@@ -94,7 +125,6 @@ def usePreTrain(args):
     keras.experimental.export_saved_model(model, args.save_dir)
 
     
-
 
 if __name__ == "__main__":
     args = options.parseArguments()
